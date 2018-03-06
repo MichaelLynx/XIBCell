7799a1bbf4b6b76f946e8f725037b92519274df4
BlackSage <1216831710@qq.com>
2018-03-06 16:18:07 +0800

    Initial Commit

---
+++ XIBCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XIBCell
+//
+//  Created by Admin on 2018/3/6.
+//  Copyright © 2018年 DB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XIBCell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XIBCell
+//
+//  Created by Admin on 2018/3/6.
+//  Copyright © 2018年 DB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
